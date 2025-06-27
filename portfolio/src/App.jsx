3d20d4609d6f393b600e5d193846eb88a9422017
Brandon Yang <brandon.yang1210@gmail.com>
2025-06-27 14:50:17 -0400

    added vercel analytics, and added more to the about section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import GalleryList from './pages/GalleryPage'
 import GalleryPage from './components/PhotoList'
 import HomePage from './pages/HomePage'
+import { Analytics } from "@vercel/analytics/react"
 // import ContactPage from './pages/ContactPage'
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/galleries" element={<GalleryList />} />
